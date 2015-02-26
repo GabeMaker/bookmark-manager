f36d6011876a71791f6695082cb556a72435b504
gabe <GabeMaker@users.noreply.github.com>
2015-02-26 15:21:07 +0000

    complete password confirmation

--- app/server.rb
+++ app/server.rb
@@ -38,7 +38,9 @@ class BMarkManager < Sinatra::Base
   end
 
   post '/users' do
-    user = User.create(:email => params[:email], :password => params[:password])
+    user = User.create(:email => params[:email],
+                       :password => params[:password],
+                       :password_confirmation => params[:password_confirmation])
     session[:user_id] = user.id
     redirect to('/')
   end
